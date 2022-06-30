9d9176d0b034c934e5d1292c6393c9f7d85e794b
Eleni693 <Elenihundie@yahoo.com>
2022-06-30 10:37:38 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,8 @@
  *  * main - Entry point
  *   * Return: Always 0 (Success)
  *    */
-int main(void)
-{
+int main(){
+	
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	return 0
 }
