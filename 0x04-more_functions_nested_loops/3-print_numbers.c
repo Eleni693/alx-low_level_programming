fbc02c81bb867fbabf406966069914a6460f4dfe
Eleni693 <Elenihundie@yahoo.com>
2022-07-07 09:25:27 -0700

    print

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,12 +6,11 @@
 **/
 void print_numbers(void)
 {
-		int n = 0;
-
-			while (n < 10)
-					{
-								_putchar(n + '0');
-										n += 1;
-											}
-				_putchar('\n');
+int n = 0;
+while (n < 10)
+{
+_putchar(n + '0');
+n += 1;
+}
+_putchar('\n');
 }
