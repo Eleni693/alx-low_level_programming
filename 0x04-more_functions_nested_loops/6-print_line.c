d48134e67d9eb60db53a8aa622371ebcdc3f36af
Eleni693 <Elenihundie@yahoo.com>
2022-07-07 09:56:10 -0700

    lineprint

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,17 +6,16 @@
 **/
 void print_line(int n)
 {
-		int i = 0;
-
-			while (i < n)
-					{
-								if (n > 0)
-											{
-															_putchar(95);
-																		i += 1;
-																				}
-										else
-														_putchar('\n');
-											}
-				_putchar('\n');
+int i = 0;
+while (i < n)
+{
+if (n > 0)
+{
+_putchar(95);
+i += 1;
+}
+else
+_putchar('\n');
+}
+_putchar('\n');
 }
