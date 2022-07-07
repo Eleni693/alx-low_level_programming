10b5b768196156ec92f8585ec9636f8e284ab11a
Eleni693 <Elenihundie@yahoo.com>
2022-07-07 10:05:37 -0700

    square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,19 +6,18 @@
 **/
 void print_square(int size)
 {
-		int col, row;
-
-			if (size > 0)
-					{
-								for (row = 0; row < size; row++)
-											{
-															for (col = 0; col < size; col++)
-																			{
-																								_putchar(35);
-																											}
-																		_putchar('\n');
-																				}
-									}
-				else
-							_putchar('\n');
+int col, row;
+if (size > 0)
+{
+for (row = 0; row < size; row++)
+{
+for (col = 0; col < size; col++)
+{
+_putchar(35);
+}
+_putchar('\n');
+}
+}
+else
+_putchar('\n');
 }
